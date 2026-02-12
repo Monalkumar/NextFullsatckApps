5327f3f0e51e933a2cce387ff68e2bbb746703a7
MonalKumar <monalshiv123@gmail.com>
2026-02-12 23:03:01 +0530

    added shadcdn Ui and configurations

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
 
     <div>
-      <h1>hello world</h1>
+      
+      <h1>Monal&apos;s Universe now You are entering in different environment</h1>
+      <Button>Click Me</Button>
+      
     </div>
   );
 }
+  
